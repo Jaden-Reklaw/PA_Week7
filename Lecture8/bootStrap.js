4ec645d8c5ede332ca4842672e0d7f7460ab9fe1
Jordan Walker <jwalker117@gmail.com>
2020-03-07 19:38:18 -0600

    Adding Lecture 8

---
+++ Lecture8/bootStrap.js
@@ -0,0 +1,22 @@
+//Test
+console.log("bootStrap.js");
+
+/*
+	Bootstrap
+	Download from Bootstrap or use CDN
+	Remember to cdn the correct jquery file
+
+	Bootstrap is used for styling.
+	Mary likes the grid system in bootstap
+*/
+
+
+
+
+
+
+
+
+
+
+
